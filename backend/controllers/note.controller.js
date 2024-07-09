41214902175388f8a6f1d302f059919296537d09
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-09 11:49:18 +0430

    create update note api

--- backend/controllers/note.controller.js
+++ backend/controllers/note.controller.js
@@ -2,22 +2,60 @@ import Note from "../models/note.model.js";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-export const addNewNote = async(req, res) => {
+export const addNewNote = async (req, res) => {
   const { title, content, tags } = req.body;
   const { id } = req.user;
 
   try {
     const note = await Note.create({
-        title,
-        content,
-        tags: tags || [],
-        userId: id
-    })
+      title,
+      content,
+      tags: tags || [],
+      userId: id,
+    });
 
     res.status(201).json({
-        message: "Note added successfully!",
-        data: note
-    })
+      message: "Note added successfully!",
+      data: note,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Internal Server Error",
+      message: error.message,
+    });
+  }
+};
+
+export const updateNote = async (req, res) => {
+  const { title, content, tags, isPinned } = req.body;
+  const { id } = req.user;
+  const { noteId } = req.params;
+  try {
+    let note = await Note.findOne({ _id: noteId, userId: id });
+
+    if (!note) {
+      return res.status(404).json({
+        message: "Note not found",
+      });
+    }
+
+    if (!title && !content && !tags) {
+      return res.status(400).json({
+        message: "No changes provided",
+      });
+    }
+
+    note.title = title || note.title;
+    note.content = content || note.content;
+    note.tags = tags || note.tags;
+    note.isPinned = isPinned || note.isPinned;
+
+    note = await note.save();
+
+    res.status(200).json({
+      message: "Note updated successfully!",
+      data: { note },
+    });
   } catch (error) {
     res.status(500).json({
       error: "Internal Server Error",
--- backend/routes/note.route.js
+++ backend/routes/note.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addNewNote } from '../controllers/note.controller.js';
+import { addNewNote, updateNote } from '../controllers/note.controller.js';
 import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 router.post('/add', verifyToken, addNewNote)
+router.patch('/update/:noteId', verifyToken, updateNote)
 
 
 export default router;
