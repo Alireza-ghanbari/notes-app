4cb1db15778ac96219c5289cc83030ba9a8bc38c
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-09 12:25:10 +0430

    create pinned note api

--- backend/controllers/note.controller.js
+++ backend/controllers/note.controller.js
@@ -99,3 +99,38 @@ export const deleteNote = async (req, res) => {
     });
   }
 };
+
+export const updatePinned = async (req, res) => {
+    const { isPinned } = req.body;
+    const { id } = req.user;
+    const { noteId } = req.params;
+    try {
+      let note = await Note.findOne({ _id: noteId, userId: id });
+  
+      if (!note) {
+        return res.status(404).json({
+          message: "Note not found",
+        });
+      }
+  
+    //   if (isPinned === '') {
+    //     return res.status(400).json({
+    //       message: "No changes provided (isPinned) not found",
+    //     });
+    //   }
+  
+      note.isPinned = isPinned || false
+  
+      note = await note.save();
+  
+      res.status(200).json({
+        message: "Note updated successfully!",
+        data: { note },
+      });
+    } catch (error) {
+      res.status(500).json({
+        error: "Internal Server Error",
+        message: error.message,
+      });
+    }
+  };
--- backend/routes/note.route.js
+++ backend/routes/note.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addNewNote, deleteNote, getAllNote, updateNote } from '../controllers/note.controller.js';
+import { addNewNote, deleteNote, getAllNote, updateNote, updatePinned } from '../controllers/note.controller.js';
 import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post('/add', verifyToken, addNewNote)
 router.patch('/update/:noteId', verifyToken, updateNote)
 router.get('/', verifyToken, getAllNote)
 router.delete('/delete/:noteId', verifyToken, deleteNote)
+router.patch('/pinned/:noteId', verifyToken, updatePinned)
 
 
 export default router;
