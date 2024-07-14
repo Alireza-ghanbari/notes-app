4e0922210710dd6cfd5eb3b6536f671f189c821e
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-07-14 22:13:12 +0430

    deploy config

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 mongoose
   .connect(process.env.DATA_BASE_URI)
@@ -12,6 +14,14 @@ mongoose
     console.log("DataBase Connected");
   })
   .catch((err) => console.log(err));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
